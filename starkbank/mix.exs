213522ed6c274b4e08150c859c9b4b6fd0b27714
Caio Dottori <caio.dottori@starkbank.com>
2020-03-23 19:57:45 -0300

    updated mix.exs

--- starkbank/mix.exs
+++ starkbank/mix.exs
@@ -1,28 +1,45 @@
-defmodule Starkbank.MixProject do
+defmodule StarkBank.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :starkbank,
-      version: "0.1.0",
+      name: :starkbank,
+      version: "2.0.0",
+      homepage_url: "https://starkbank.com",
+      source_url: "https://github.com/starkbank/sdk-elixir",
+      description: description(),
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  defp package do
     [
-      extra_applications: [:logger]
+      maintainers: ["Stark Bank"],
+      licenses: [:MIT],
+      links: %{
+        "StarkBank" => "https://starkbank.com",
+        "GitHub" => "https://github.com/starkbank/sdk-elixir"
+      }
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp description do
+    "SDK to make integrations with the Stark Bank API easier."
+  end
+
+  def application do
+    []
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:starkbank_ecdsa, "~> 0.0.1"},
+      {:jason, "~> 1.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
