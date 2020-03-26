5ab4a4b014f718cc3f19e777d7304eccee221456
Caio Dottori <caio.dottori@starkbank.com>
2020-03-26 18:49:14 -0300

    fixed URL query builder

--- starkbank/lib/utils/url.ex
+++ starkbank/lib/utils/url.ex
@@ -1,4 +1,9 @@
 defmodule StarkBank.Utils.URL do
+
+  @moduledoc false
+
+  alias StarkBank.Utils.Case, as: Case
+
   def get_url(environment, version, path, query) do
     base_url(environment) ++ version ++ '/'
      |> add_path(path)
@@ -21,7 +26,7 @@ defmodule StarkBank.Utils.URL do
   end
 
   defp add_query(endpoint, query) do
-    list = for {k, v} <- query, !is_nil(v), do: "#{k}=#{to_query_string(v)}"
+    list = for {k, v} <- query, !is_nil(v), do: "#{k |> query_key}=#{v |> query_argument}"
 
     if length(list) > 0 do
       endpoint ++ to_charlist("?" <> String.replace(Enum.join(list, "&"), " ", "%20"))
@@ -30,13 +35,19 @@ defmodule StarkBank.Utils.URL do
     end
   end
 
-  defp to_query_string(value) when is_list(value) or is_tuple(value) do
+  defp query_key(key) do
+    key
+     |> to_string
+     |> Case.snake_to_camel
+  end
+
+  defp query_argument(value) when is_list(value) or is_tuple(value) do
     value
      |> Enum.map(fn v -> to_string(v) end)
      |> Enum.join(",")
   end
 
-  defp to_query_string(value) do
+  defp query_argument(value) do
     value
   end
 end
