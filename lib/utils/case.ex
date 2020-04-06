bcfb9413d5ae72ff1563c2bb79ca3e7851c56c4a
Caio Dottori <caio.dottori@starkbank.com>
2020-04-06 11:11:04 -0300

    fixed camel_to_snake with numbers

--- lib/utils/case.ex
+++ lib/utils/case.ex
@@ -1,8 +1,38 @@
 defmodule StarkBank.Utils.Case do
   @moduledoc false
 
+  def camel_to_kebab(string) do
+    string
+     |> camel_to_snake
+     |> String.replace("_", "-")
+  end
+
   def camel_to_snake(string) do
-    Macro.underscore(string)
+    string
+      |> String.graphemes
+      |> camel_to_snake_graphemes
+      |> strip_underscore
+      |> Enum.join
+  end
+
+  defp camel_to_snake_graphemes([letter | rest]) do
+    cond do
+      letter == letter |> String.upcase -> ["_" | [String.downcase(letter) | camel_to_snake_graphemes(rest)]]
+      :error != Integer.parse(letter) -> ["_" | [letter | camel_to_snake_graphemes(rest)]]
+      true -> [letter | camel_to_snake_graphemes(rest)]
+    end
+  end
+
+  defp camel_to_snake_graphemes([]) do
+    []
+  end
+
+  defp strip_underscore([letter | rest]) when letter == "_" do
+    rest
+  end
+
+  defp strip_underscore(string) do
+    string
   end
 
   def snake_to_camel(string) do
@@ -22,11 +52,5 @@ defmodule StarkBank.Utils.Case do
 
   defp snake_to_camel_graphemes([]) do
     []
-  end
-
-  def camel_to_kebab(string) do
-    string
-     |> Macro.underscore()
-     |> String.replace("_", "-")
   end
 end
