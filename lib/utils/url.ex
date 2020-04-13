deae12b71cdc049b74ca27e5262b3559570449e9
Caio Dottori <caio.dottori@starkbank.com>
2020-04-13 15:46:47 -0300

    adopted API.cast_json in urlencode

--- lib/utils/url.ex
+++ lib/utils/url.ex
@@ -2,7 +2,7 @@ defmodule StarkBank.Utils.URL do
 
   @moduledoc false
 
-  alias StarkBank.Utils.Case, as: Case
+  alias StarkBank.Utils.API, as: API
 
   def get_url(environment, version, path, query) do
     base_url(environment) ++ version ++ '/'
@@ -26,7 +26,7 @@ defmodule StarkBank.Utils.URL do
   end
 
   defp add_query(endpoint, query) do
-    list = for {k, v} <- query, !is_nil(v), do: "#{k |> query_key}=#{v |> query_argument}"
+    list = for {k, v} <- query |> API.cast_json_to_api_format, !is_nil(v), do: "#{k |> query_key}=#{v |> query_argument}"
 
     if length(list) > 0 do
       endpoint ++ to_charlist("?" <> String.replace(Enum.join(list, "&"), " ", "%20"))
@@ -38,7 +38,6 @@ defmodule StarkBank.Utils.URL do
   defp query_key(key) do
     key
      |> to_string
-     |> Case.snake_to_camel
   end
 
   defp query_argument(value) when is_list(value) or is_tuple(value) do
