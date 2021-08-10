6f68b52a58a4dc5abd4433e103f4643dc16f8ed1
Caio Dottori <caio.dottori@starkbank.com>
2021-08-10 14:49:24 -0300

    Fix minor warnings in Institution resource

--- lib/institution/institution.ex
+++ lib/institution/institution.ex
@@ -1,22 +1,19 @@
 defmodule StarkBank.Institution do
     alias __MODULE__, as: Institution
     alias StarkBank.Utils.Rest
-    alias StarkBank.Utils.Check
-    alias StarkBank.Utils.API
-    alias StarkBank.Transfer
     alias StarkBank.User.Project
     alias StarkBank.User.Organization
     alias StarkBank.Error
-  
+
     @moduledoc """
     Groups Institution related functions
     """
-  
+
     @doc """
     This resource is used to get information on the institutions that are recognized by the Brazilian Central Bank.
     Besides the display name and full name, they also include the STR code (used for TEDs) and the SPI Code
     (used for Pix) for the institutions. Either of these codes may be empty if the institution is not registered on
-    that Central Bank service.  
+    that Central Bank service.
 
     ## Attributes:
       - `:display_name` [string]: short version of the institution name that should be displayed to end users. ex: "Stark Bank"
@@ -27,17 +24,17 @@ defmodule StarkBank.Institution do
     defstruct [:display_name, :name, :spi_code, :str_code]
 
     @type t() :: %__MODULE__{}
-  
+
     @doc """
     Receive a list of Institution objects that are recognized by the Brazilian Central bank for Pix and TED transactions
-  
+
     ## Options:
       - `:limit` [integer, default nil]: maximum number of objects to be retrieved. Unlimited if nil. ex: 35
       - `:search` [string, default nil]: part of the institution name to be searched. ex: "stark"
       - `:spi_codes` [list of strings, default nil]: list of SPI (Pix) codes to be searched. ex: ["20018183"]
       - `:str_codes` [list of strings, default nil]: list of STR (TED) codes to be searched. ex: ["260"]
       - `:user` [Organization/Project, default nil]: Organization or Project struct returned from StarkBank.project(). Only necessary if default project or organization has not been set in configs.
-  
+
     ## Return:
       - list of Institution objects with updated attributes
     """
@@ -56,11 +53,11 @@ defmodule StarkBank.Institution do
                any)
     def query(options \\ []) do
       case Rest.get_page(resource(), options) do
-        {:ok, {cursor, entities}} -> {:ok, entities}
+        {:ok, {_cursor, entities}} -> {:ok, entities}
         {:error, error} -> {:error, error}
       end
     end
-  
+
     @doc """
     Same as query(), but it will unwrap the error tuple and raise in case of errors.
     """
@@ -75,7 +72,7 @@ defmodule StarkBank.Institution do
     def query!(options \\ []) do
       Rest.get_page!(resource(), options) |> elem(1)
     end
-  
+
     @doc false
     def resource() do
       {
@@ -83,7 +80,7 @@ defmodule StarkBank.Institution do
         &resource_maker/1
       }
     end
-  
+
     @doc false
     def resource_maker(json) do
       %Institution{
