3fb24f3d649e757a7d5a3c3efff3b657c37492df
Felipe Sueto <felipe.sueto@starkbank.com>
2021-11-10 19:09:58 -0300

    Update starkbank-ecdsa library version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule StarkBank.MixProject do
 
   defp deps do
     [
-      {:starkbank_ecdsa, "~> 1.0.1"},
+      {:starkbank_ecdsa, "~> 1.1.0"},
       {:jason, "~> 1.1"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
